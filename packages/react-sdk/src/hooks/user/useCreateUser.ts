e6dc5b386fd09b1e2d375cc2dd15031becc03cc8
Vijay Gupta <vjgupta57@gmail.com>
2023-02-08 16:32:03 +0530

    add: hook for create user

---
+++ packages/react-sdk/src/hooks/user/useCreateUser.ts
@@ -0,0 +1,39 @@
+import { SDK } from "@gumhq/sdk";
+import { useState, useEffect, useCallback } from "react";
+import { PublicKey } from "@solana/web3.js";
+
+const useCreateUser = (sdk: SDK, owner: PublicKey) => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const createUser = useCallback(
+    async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        let data;
+        data = await sdk.user.create(owner);
+
+        setUser(data);
+      } catch (err: any) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }, [sdk, owner]);
+
+  useEffect(() => {
+  createUser();
+  }, [createUser]);
+
+  return { 
+    instructionMethodBuilder: user ? user.instructionMethodBuilder : undefined,
+    userPDA: user ? user.userPDA : undefined,
+    loading, 
+    error 
+  };
+};
+
+export default useCreateUser;
