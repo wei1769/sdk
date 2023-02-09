ccf6387015087d87be89bde6f864704712dda22e
Vijay Gupta <vjgupta57@gmail.com>
2023-02-10 04:21:46 +0530

    fix: pass wallet as a parameter to useGum hook

--- packages/react-sdk/src/hooks/useGum.ts
+++ packages/react-sdk/src/hooks/useGum.ts
@@ -1,36 +1,17 @@
-import { GraphQLClient } from "graphql-request";
-import { useAnchorWallet, AnchorWallet } from "@solana/wallet-adapter-react";
 import { useState, useEffect } from 'react';
-import { SDK } from "@gumhq/sdk";
-import { Connection, ConfirmOptions, Cluster } from "@solana/web3.js";
+import { GraphQLClient } from 'graphql-request';
+import { AnchorWallet } from '@solana/wallet-adapter-react';
+import { SDK } from '@gumhq/sdk';
+import { Connection, ConfirmOptions, Cluster } from '@solana/web3.js';
 
-const useGum = (connection: Connection, opts: ConfirmOptions, cluster: Cluster, graphqlClient?: GraphQLClient) => {
-  const anchorWallet = useAnchorWallet() as AnchorWallet;
+const useGum = (wallet: AnchorWallet, connection: Connection, opts: ConfirmOptions, cluster: Cluster, graphqlClient?: GraphQLClient) => {
   const [sdk, setSdk] = useState<SDK | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-
+  
   useEffect(() => {
-    try {
-      if (!anchorWallet) {
-        throw new Error("Wallet not connected");
-      }
+    setSdk(new SDK(wallet, connection, opts, cluster, graphqlClient));
+  }, [wallet]);
 
-      const sdkInstance = new SDK(
-        anchorWallet,
-        connection,
-        opts,
-        cluster,
-        graphqlClient
-      );
-
-      setSdk(sdkInstance);
-    } catch (err: any) {
-      setError(err);
-    }
-
-  }, [anchorWallet, connection, opts, cluster, graphqlClient]);
-
-  return { sdk, error };
+  return sdk;
 };
 
 export default useGum;
